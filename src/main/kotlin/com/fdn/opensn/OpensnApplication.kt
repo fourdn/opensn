009d864a8084ddb8a69d31b822b156b01da0872f
IvanKayukoff <ivankayukoff@gmail.com>
2019-05-11 00:25:07 +0300

    feat: initial commit

---
+++ src/main/kotlin/com/fdn/opensn/OpensnApplication.kt
@@ -0,0 +1,11 @@
+package com.fdn.opensn
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpensnApplication
+
+fun main(args: Array<String>) {
+	runApplication<OpensnApplication>(*args)
+}
