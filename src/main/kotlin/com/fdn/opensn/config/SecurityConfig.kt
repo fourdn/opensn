babf77b52e1be02c6601b75529689bf8023a960c
IvanKayukoff <ivankayukoff@gmail.com>
2019-06-30 04:04:10 +0300

    feat: add custom authentication handlers

--- src/main/kotlin/com/fdn/opensn/config/SecurityConfig.kt
+++ src/main/kotlin/com/fdn/opensn/config/SecurityConfig.kt
@@ -1,6 +1,9 @@
 package com.fdn.opensn.config
 
 import com.fdn.opensn.domain.UserRole
+import com.fdn.opensn.security.RESTAuthenticationEntryPoint
+import com.fdn.opensn.security.RESTAuthenticationFailureHandler
+import com.fdn.opensn.security.RESTAuthenticationSuccessHandler
 import com.fdn.opensn.service.SimpleAuthenticationProvider
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
@@ -17,20 +20,27 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 @ComponentScan("com.fdn.opensn.service")
 class SecurityConfig @Autowired
 constructor(
-    private val simpleAuthProvider: SimpleAuthenticationProvider
+    private val simpleAuthProvider: SimpleAuthenticationProvider,
+    private val authenticationEntryPoint: RESTAuthenticationEntryPoint,
+    private val authenticationFailureHandler: RESTAuthenticationFailureHandler,
+    private val authenticationSuccessHandler: RESTAuthenticationSuccessHandler
 ) : WebSecurityConfigurerAdapter() {
 
   override fun configure(http: HttpSecurity) {
     http.csrf().disable() // TODO configure csrf
-        .authorizeRequests()
-        .antMatchers("/").permitAll()
-        .antMatchers("/main").hasRole(UserRole.USER.toString())
-        .and().formLogin().defaultSuccessUrl("/main")
-        .and().logout().logoutUrl("/logout").logoutSuccessUrl("/login")
+
+    http.authorizeRequests().antMatchers("/").permitAll()
+    http.authorizeRequests().antMatchers("/main").hasRole(UserRole.USER.toString())
+
+    http.exceptionHandling().authenticationEntryPoint(authenticationEntryPoint)
+    http.formLogin().successHandler(authenticationSuccessHandler)
+    http.formLogin().failureHandler(authenticationFailureHandler)
+
+    http.logout().logoutUrl("/logout").logoutSuccessUrl("/login")
   }
 
   /**
-   * Add authentication based upon the custom AuthenticationProvider
+   * Adds authentication based upon the custom AuthenticationProvider.
    */
   override fun configure(auth: AuthenticationManagerBuilder) {
     auth.authenticationProvider(simpleAuthProvider)
