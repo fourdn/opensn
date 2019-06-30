a28f8442174c7f5d66e76665b7b8a3ca59388867
IvanKayukoff <ivankayukoff@gmail.com>
2019-06-30 03:20:47 +0300

    feat: add registration service

--- src/main/kotlin/com/fdn/opensn/config/SecurityConfig.kt
+++ src/main/kotlin/com/fdn/opensn/config/SecurityConfig.kt
@@ -6,15 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.core.userdetails.User
-import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 
 @Configuration
 @EnableWebSecurity
@@ -34,7 +30,7 @@ constructor(
   }
 
   /**
-   * Add authentication based upon the custom {@link AuthenticationProvider}
+   * Add authentication based upon the custom AuthenticationProvider
    */
   override fun configure(auth: AuthenticationManagerBuilder) {
     auth.authenticationProvider(simpleAuthProvider)
---
+++ src/main/kotlin/com/fdn/opensn/dto/RegistrationResponseDto.kt
@@ -0,0 +1,7 @@
+package com.fdn.opensn.dto
+
+class RegistrationResponseDto(
+    val success: Boolean,
+    val message: String,
+    val statusCode: Int
+)
