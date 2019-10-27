8c73be0ad89031aa5ceaf4ff0eacba328e06345e
Vanchesky <ivankayukoff@gmail.com>
2019-10-27 15:00:17 +0300

    feat: add references from publication to user

--- src/main/kotlin/com/fdn/opensn/domain/Publication.kt
+++ src/main/kotlin/com/fdn/opensn/domain/Publication.kt
@@ -1,15 +1,27 @@
 package com.fdn.opensn.domain
 
+import com.fasterxml.jackson.annotation.JsonBackReference
 import javax.persistence.*
 
 @Entity
 @Table(name = "publication")
-data class Publication(val targetId: Long = 0,
-                       val authorId: Long = 0,
-                       @Enumerated(EnumType.STRING)
-                       val privateStatus: PrivateStatus = PrivateStatus.PRIVATE,
-                       @Enumerated(EnumType.STRING)
-                       val contentType: ContentType = ContentType.TEXT) {
+data class Publication(
+    @JsonBackReference
+    @ManyToOne
+    @JoinColumn(name = "target_id")
+    val target: User = User(),
+
+    @JsonBackReference
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    val author: User = User(),
+
+    @Enumerated(EnumType.STRING)
+    val privateStatus: PrivateStatus = PrivateStatus.PRIVATE,
+
+    @Enumerated(EnumType.STRING)
+    val contentType: ContentType = ContentType.TEXT
+) {
 
   @Id
   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "publication_gen")
--- src/main/kotlin/com/fdn/opensn/domain/User.kt
+++ src/main/kotlin/com/fdn/opensn/domain/User.kt
@@ -4,13 +4,27 @@ import javax.persistence.*
 
 @Entity
 @Table(name = "usr")
-data class User(val username: String = "",
-                val password: String = "",
+data class User(
+    val username: String = "",
+    val password: String = "",
 
-                @Enumerated(EnumType.STRING)
-                @ElementCollection(targetClass = UserRole::class)
-                @CollectionTable(name = "roles", joinColumns = [JoinColumn(name = "role_id")])
-                val roles: Set<UserRole> = emptySet()) {
+    @Enumerated(EnumType.STRING)
+    @ElementCollection(targetClass = UserRole::class)
+    @CollectionTable(name = "roles", joinColumns = [JoinColumn(name = "role_id")])
+    val roles: Set<UserRole> = emptySet(),
+
+    /**
+     * Contains all publications on the user's wall.
+     */
+    @OneToMany(mappedBy = "target")
+    val wallPublications: Set<Publication> = emptySet(),
+
+    /**
+     * Contains all publications that created by this user.
+     */
+    @OneToMany(mappedBy = "author")
+    val ownedPublications: Set<Publication> = emptySet()
+) {
 
   @Id
   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_gen")
